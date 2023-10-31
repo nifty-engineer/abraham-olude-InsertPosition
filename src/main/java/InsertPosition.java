eeb5bf46378c72404e3f2bf11c3524adfa09461c
Abraham Olude <abraham.olude.ao@gmail.com>
2023-10-31 06:35:11 +0000

    Complete Coding Challenge: Insert Position

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,29 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+
+        int highestIndex = nums.length - 1;
+        int lowestIndex = 0;
+
+        while (highestIndex >= lowestIndex && highestIndex - lowestIndex > 1) {
+
+            int midIndex = (highestIndex + lowestIndex) / 2;
+
+            if (nums[midIndex] > target) {
+                highestIndex = midIndex;
+            }
+            else if (nums[midIndex] < target) {
+                lowestIndex = midIndex;
+            }
+        }
+
+        if (target < nums[lowestIndex]) {
+            return lowestIndex;
+        }
+        else if (target > nums[lowestIndex] && target < nums[highestIndex]) {
+            return highestIndex;
+        }
+
+            return highestIndex + 1;       
     }
 }
